20ceb27de5402268c64dc51cb28a8ba626be7dd2
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2026-01-29 23:08:33 +1100

    Update serialisers.R

--- R/serialisers.R
+++ R/serialisers.R
@@ -1,19 +1,22 @@
 #' @export
 register_html_serialiser <- function() {
-    old_serialiser <- plumber2::get_serializers("html")
-
-    plumber2::register_serializer(
-        "html",
-        function(...) {
-            function(x) {
-                if (inherits(x, c("shiny.tag", "shiny.tag.list"))) {
-                    doRenderTags(x) |>
-                        as.character()
-                } else {
-                    old_serialiser(x)
-                }
-            }
+    eval(substitute(
+        {
+            plumber2::register_serializer(
+                "html",
+                function(...) {
+                    function(x) {
+                        if (inherits(x, c("shiny.tag", "shiny.tag.list"))) {
+                            htmltools::doRenderTags(x) |>
+                                as.character()
+                        } else {
+                            old_serialiser(x)
+                        }
+                    }
+                },
+                mime_type = "text/html"
+            )
         },
-        mime_type = "text/html"
-    )
+        list(old_serialiser = plumber2::get_serializers("html")[[1L]])
+    ))
 }
