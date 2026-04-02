0d74186e1c1db12e8745e72529950e1ad68eec62
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2026-04-03 00:55:21 +1100

    endpoints & redirect

---
+++ tests/testthat/test-url.R
@@ -0,0 +1,57 @@
+test_that("redirect works", {
+    suppressMessages({
+        api <- plumber2::api() |>
+            plumber2::api_get("/", function(response) {
+                redirect(response, "/foo")
+            }) |>
+            plumber2::api_get("/foo", function() {
+                "Hello world!"
+            })
+    })
+
+    res <- faux_request(api, path = "")
+
+    expect_identical(res$status, 303L)
+    expect_identical(res$headers$location, "/foo")
+})
+
+test_that("redirect after works", {
+    suppressMessages({
+        api <- plumber2::api() |>
+            plumber2::api_get("/", function(response) {
+                redirect(response, "/foo", after = 3L)
+            }) |>
+            plumber2::api_get("/foo", function() {
+                "Hello world!"
+            })
+    })
+
+    res <- faux_request(api, path = "")
+
+    expect_identical(res$status, 200L)
+    expect_identical(res$headers$refresh, "3; url=/foo")
+})
+
+test_that("endpoints work", {
+    suppressMessages({
+        api <- plumber2::api() |>
+            plumber2::api_get("/foo", function(response) {
+                "foo"
+            }) |>
+            plumber2::api_get("/bar", function() {
+                "bar"
+            }) |>
+            plumber2::api_get("/baz", function() {
+                "baz"
+            }) |>
+            plumber2::api_delete("/baz/:id", function() {
+                "baz"
+            })
+    })
+
+    eps <- endpoints("default", api = api, refresh = TRUE)
+
+    expect_length(eps, 3L)
+    expect_identical(eps$foo(), "/foo")
+    expect_identical(eps$baz$delete(id = 1L), "/baz/1")
+})
